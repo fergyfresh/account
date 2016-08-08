033a0c8baf59f85a529c786a069712845d4f92f7
Billy Ferguson <william.d.ferg@gmail.com>
2016-08-08 16:55:05 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'users#index', as: :authenticated_root
+      root 'records#index', as: :authenticated_root
     end
 
     unauthenticated do
