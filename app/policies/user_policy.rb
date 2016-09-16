5602d52fbcc7505ca982adb536dd50566c3eebe4
Billy Ferguson <william.d.ferg@gmail.com>
2016-09-16 09:52:13 -0400

    Create user_policy.rb

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,15 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end
+
+  def destroy?
+    user.admin?
+  end
+end
