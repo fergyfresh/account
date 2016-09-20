07d70263cfbfd899179aa5d61a903ab27a74ec70
Billy Ferguson <william.d.ferg@gmail.com>
2016-09-20 12:56:55 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
   end
   
   def promote
-    @user = current_user
-    @users = User.all.where("id != ?", current_user.id).paginate(page: params[:page])
-  end  
+    @user = User.find(params[:employee_id])
+    @user.supervisor = true
+    @user.save
+    respond_to do |format|
+      format.html { redirect_to action: "index", controller: "users" }
+      format.js
+    end
+  end 
 end
