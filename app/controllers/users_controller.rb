0e72cf2a2d7c548c83d7a65966a57fa37ddef633
Billy Ferguson <william.d.ferg@gmail.com>
2016-09-20 12:55:31 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @user = current_user
     @users = User.all.where("id != ?", current_user.id).paginate(page: params[:page])
   end
+  
+  def promote
+    @user = current_user
+    @users = User.all.where("id != ?", current_user.id).paginate(page: params[:page])
+  end  
 end
