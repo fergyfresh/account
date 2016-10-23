c66ea715358eb94f99a4168d7d8fca7c7d000bc2
Billy <william.d.ferg@gmail.com>
2016-10-23 11:34:06 -0400

    added prject model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920155517) do
+ActiveRecord::Schema.define(version: 20161023153118) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "records", force: :cascade do |t|
     t.string   "title"
