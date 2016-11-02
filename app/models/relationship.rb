242ec3d2983078f2b4a513d2652af1b898ac6ad5
Billy Ferguson <william.d.ferg@gmail.com>
2016-11-02 10:46:22 -0400

    Update relationship.rb

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,8 @@
 class Relationship < ActiveRecord::Base
   belongs_to :supervisor, class_name: "User"
   belongs_to :employee, class_name: "User"
+  belongs_to :project, class_name: "Project"
   validates :supervisor_id, presence: true
   validates :employee_id, presence: true
+  validates :project_id, presence: true
 end
