91de343eb304d75515185eabaf2f47658b09dbe3
Billy Ferguson <william.d.ferg@gmail.com>
2016-11-23 09:17:05 -0500

    Update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,12 +8,10 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     @pm = User.find(@project.user_id)
     @employees = {}
-    @supervisors ||= []
-    @supervisors << User.all.where("id = ?", Relationship.all.where("supervisor_id = ?", @project.user_id).pluck(:supervisor_id))    
+    @supervisors = User.all.where("id = ?", Relationship.all.where("supervisor_id = ?", @project.user_id).pluck(:supervisor_id))    
     @supervisor_ids = Relationship.all.where("supervisor_id = ?", @project.user_id).pluck(:supervisor_id)
     for supervisor in @supervisor_ids
-      @employees[supervisor] ||= []
-      @employees[supervisor] << User.all.where("id = ?", Relationship.all.where("supervisor_id = ?", supervisor).pluck(:employee_id))
+      @employees[supervisor] = User.all.where("id = ?", Relationship.all.where("supervisor_id = ?", supervisor).pluck(:employee_id))
     end
   end
 
