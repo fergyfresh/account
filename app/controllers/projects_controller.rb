1ad9c6fd6246a9b9aeb6397dafea2128a26327a1
Billy Ferguson <william.d.ferg@gmail.com>
2016-11-28 13:05:57 -0500

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @pm = User.find(@project.user_id)
-    @supervisor_ids = Relationship.all.where("supervisor_id = ?", @project.user_id).pluck(:supervisor_id)
+    @supervisor_ids = Relationship.all.where("supervisor_id = ?", @project.user_id).pluck(:employee_id)
     @supervisors = User.all.where("id = ?", @supervisor_ids)
     @employees = {}
     for supervisor in @supervisor_ids
