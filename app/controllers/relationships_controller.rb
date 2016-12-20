df0023f2c969f4e929c70e13c2fef34eed5d768a
Billy Ferguson <william.d.ferg@gmail.com>
2016-12-20 14:24:18 -0500

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,14 @@ class RelationshipsController < ApplicationController
   def create
     @supervisor = User.find(params[:supervisor_id])
     @employee = User.find(params[:employee_id])
-    @supervisor.hire(@employee)
+    @supervisor.hire(@employee, params[:project_id])
     render json: @employee
   end
 
   def destroy
     @supervisor = User.find(params[:supervisor_id])
     @employee = User.find(params[:employee_id])
-    @supervisor.fire(@employee)
+    @supervisor.fire(@employee, , params[:project_id])
     render json: @employee
   end
 end
