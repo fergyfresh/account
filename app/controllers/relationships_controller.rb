47074a965fa9f53aacedcf4a1dea7ddda91a5b52
Billy Ferguson <william.d.ferg@gmail.com>
2016-12-20 16:36:45 -0500

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,7 @@
 class RelationshipsController < ApplicationController
   def create
     @relationship = Relationship.new(supervisor_id: params[:supervisor_id], employee_id: params[:employee_id], project_id: params[:project_id])
+    @employee = User.find(params[:employee_id])
     if @relationship.save
       render json: @employee
     else
