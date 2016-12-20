a2ea369404e509c23da784d5aa8c60404656806c
Billy Ferguson <william.d.ferg@gmail.com>
2016-12-20 12:00:14 -0500

    Update relationships_controller.rb

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
-    @user = User.find(params[:employee_id])
-    current_user.hire(@user)
-    respond_to do |format|
-      format.html { redirect_to action: "index", controller: "users" }
-      format.js
-    end
+    @supervisor = User.find(params[:supervisor_id])
+    @employee = User.find(params[:employee_id])
+    @supervisor.hire(@employee)
+    render json: @employee
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).employee
-    current_user.fire(@user)
-    respond_to do |format|
-      format.html { redirect_to action: "index", controller: "users" }
-      format.js
-    end
+    @supervisor = User.find(params[:supervisor_id])
+    @employee = User.find(params[:employee_id])
+    @supervisor.fire(@employee)
+    render json: @employee
   end
 end
